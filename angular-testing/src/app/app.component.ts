737b4d18667ab9049407a029141974b5424efeaf
alefiori <alefiori97@gmail.com>
2023-06-18 20:43:25 +0100

    chore: projects cleanup

--- angular-testing/src/app/app.component.ts
+++ angular-testing/src/app/app.component.ts
@@ -1,10 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  standalone: true,
+  imports: [CommonModule],
+  template: `<h1>Angular Application</h1>`,
 })
-export class AppComponent {
-  title = 'angular-testing';
-}
+export class AppComponent {}
