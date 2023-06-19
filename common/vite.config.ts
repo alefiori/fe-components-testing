974d988a8cf4e3e7a7da8b87e0369ffb45539c9e
alefiori <alefiori97@gmail.com>
2023-06-19 15:08:12 +0100

    feat: add common library for styles and types

---
+++ common/vite.config.ts
@@ -0,0 +1,13 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: 'Common',
+    }
+  },
+  plugins: [dts()]
+})
