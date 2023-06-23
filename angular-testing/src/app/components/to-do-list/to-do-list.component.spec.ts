e9f3d60ba1f22523ce2f232c9cbaa2e4d8122a66
FrancescoSanti96 <francescosanti123@gmail.com>
2023-06-23 16:13:53 +0200

    test to-do-list

--- angular-testing/src/app/components/to-do-list/to-do-list.component.spec.ts
+++ angular-testing/src/app/components/to-do-list/to-do-list.component.spec.ts
@@ -1,1 +1,31 @@
+import { Component } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
 import { ToDoListComponent } from './to-do-list.component';
+
+@Component({
+  selector: 'app-to-do',
+  standalone: true,
+  template: `<p>To do</p>`,
+})
+class ToDoMockComponent {}
+
+describe('Test to do list', () => {
+  beforeAll(() => {
+    TestBed.configureTestingModule({
+      imports: [ToDoMockComponent],
+    });
+  });
+  it('Find List of ToDos as Title', () => {
+    const componentToDoList = TestBed.createComponent(ToDoListComponent);
+    const title = componentToDoList.nativeElement.querySelector('h2');
+    expect(title.textContent).toBe('List of ToDos');
+  });
+
+  it('How many times todo list print ToDo', () => {
+    const fixture = TestBed.createComponent(ToDoListComponent);
+    const component = fixture.componentInstance;
+    component.toDoList = [];
+    const listTodo = fixture.nativeElement.querySelectorAll('p');
+    expect(listTodo.length).toBe(0);
+  });
+});
