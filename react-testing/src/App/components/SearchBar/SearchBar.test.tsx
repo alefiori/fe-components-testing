b93a37cd56df50c564b495474c37461cea67da84
mariateresagraziano <maria.teresa.graziano@it.clara.net>
2023-06-30 08:59:10 +0200

    add test searchbar (error fireEvent)

--- react-testing/src/App/components/SearchBar/SearchBar.test.tsx
+++ react-testing/src/App/components/SearchBar/SearchBar.test.tsx
@@ -1,3 +1,16 @@
+import { fireEvent, render } from "@testing-library/react";
+import { SearchBar } from "..";
+
 describe("SearchBar component", () => {
-  it.todo("should be well tested");
+  it.skip("should change value of input element", () => {
+    const spy = jest.fn();
+    const { container } = render(
+      <SearchBar searchValue={"__VALUE__"} onSearchValueChange={spy} />
+    );
+    const input = container.querySelector("input");
+    expect(input?.value).toBe("__VALUE__");
+
+    fireEvent.change(input!, "__CHANGE__");
+    expect(input?.value).toBe("__CHANGE__");
+  });
 });
