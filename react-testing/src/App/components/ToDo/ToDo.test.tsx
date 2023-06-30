d058ed9632c008744e86590b4086d277b23d378d
mariateresagraziano <maria.teresa.graziano@it.clara.net>
2023-06-30 08:59:25 +0200

    add test toDo (error fireEvent)

--- react-testing/src/App/components/ToDo/ToDo.test.tsx
+++ react-testing/src/App/components/ToDo/ToDo.test.tsx
@@ -1,3 +1,45 @@
+import { fireEvent, render } from "@testing-library/react";
+import { ToDo as ToDoType } from "common/dist/types";
+import { ToDo } from "..";
+
+const toDoCard: ToDoType = {
+  id: 1,
+  todo: "__TODO_1__",
+  completed: false,
+  userId: 1,
+};
+
+const toDoCardCompleted: ToDoType = {
+  id: 1,
+  todo: "__TODO_1__",
+  completed: true,
+  userId: 1,
+};
+
 describe("ToDo component", () => {
-  it.todo("should be well tested");
+  it("render content of toDo", () => {
+    const { container } = render(<ToDo {...toDoCard} />);
+    const paragraph = container.querySelector("p");
+    expect(paragraph?.textContent).toBe("__TODO_1__");
+  });
+
+  it.skip("should click the card", () => {
+    const onClick = jest.fn();
+    const { container } = render(<ToDo {...toDoCard} />);
+    const card = container.querySelector("div");
+    fireEvent.click(card!);
+    expect(onClick).toHaveBeenCalled();
+  });
+
+  it("should render the card uncompleted", () => {
+    const { container } = render(<ToDo {...toDoCard} />);
+    const card = container.querySelector("div");
+    expect(card?.className).toBe("todo ");
+  });
+
+  it("should render the card completed", () => {
+    const { container } = render(<ToDo {...toDoCardCompleted} />);
+    const card = container.querySelector("div");
+    expect(card?.className).toBe("todo todo--checked");
+  });
 });
